f39b8f7741918834f96f714ac6b64d27d0e1d6c0
Kaioru <keifuurawrs@gmail.com>
2017-02-23 21:19:06 +0800

    Add Dingo and Lumen generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,6 +81,8 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Dingo\Api\Provider\LumenServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
